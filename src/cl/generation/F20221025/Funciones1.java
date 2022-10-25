e425d2a11844295bb71991c44bbb5ed0f04ffbdf
fati987 <maria.requena@mail.udp.cl>
2022-10-25 16:41:04 -0300

    metodos1

---
+++ src/cl/generation/F20221025/Funciones1.java
@@ -0,0 +1,39 @@
+package cl.generation.F20221025;
+
+import java.util.Arrays;
+
+public class Funciones1 {
+
+	public static void main(String[] args) {
+		// funciones o metodo
+
+		// llamar a un metodo
+		nombreMetodo();
+		metodo2("Fatima");
+		metodo2("Sebastian");
+		metodo3(22);
+		metodo4(4,6.9F);
+		String[]arreglito = {"1","2","3"};
+		metodo5(arreglito);
+	}
+
+	// definicion o estructura de un metodo
+	// void > no retorna ningun valor
+	public static void nombreMetodo() {
+		System.out.println("Estoy dentro del metodo inicial");
+	}
+	
+	public static void metodo2(String nombre) {
+		System.out.println("Hola soy "+ nombre);
+	}
+	public static void metodo3(Integer numero) {
+		System.out.println("Tengo "+ numero + " anos");
+	}
+	public static void metodo4(Integer numero1, Float numero2) {//se puede agregar la cantidad que necesotemos
+		System.out.println("Hola soy el numero "+ numero1);
+		System.out.println("Hola soy el numero "+ numero2);
+	}
+	public static void metodo5(String[]arreglito) {
+		System.out.println("Soy el arreglo "+Arrays.toString(arreglito));
+	}
+}
